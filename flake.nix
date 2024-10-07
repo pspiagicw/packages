77694219783cd9cf43bb49d11c815869d2bf1d2e
pspiagicw <pspiagicw@gmail.com>
2024-10-07 08:18:58 +0000

    feat: added groom for aarch64

--- flake.nix
+++ flake.nix
@@ -4,21 +4,38 @@
   outputs = {
     self,
     nixpkgs,
+    ...
   }: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
+    pkgs = {
+      x86_64-linux = import nixpkgs {system = "x86_64-linux";};
+      aarch64-linux = import nixpkgs {system = "aarch64-linux";};
+    };
 
-    groom = pkgs.buildGoModule rec {
-      name = "groom";
-      src = pkgs.fetchFromGitHub {
-        owner = "pspiagicw";
-        repo = "groom";
-        rev = "3fd54797db3a614cbc3fa43f3e29a152aa01ffde";
-        sha256 = "O37ciBvtbuZX3KNVSg7ZG02o+b0PCQ2K8hJoyo2hPsY=";
+    groom = {system}:
+      pkgs.${system}.buildGoModule rec {
+        name = "groom";
+        src = pkgs.${system}.fetchFromGitHub {
+          owner = "pspiagicw";
+          repo = "groom";
+          rev = "3fd54797db3a614cbc3fa43f3e29a152aa01ffde";
+          sha256 = "O37ciBvtbuZX3KNVSg7ZG02o+b0PCQ2K8hJoyo2hPsY=";
+        };
+
+        vendorHash = "sha256-r0ttcRQscZFrY69+EBFRLAlWgmKUXBFanQtX1OWvm48=";
       };
 
-      vendorHash = "sha256-r0ttcRQscZFrY69+EBFRLAlWgmKUXBFanQtX1OWvm48=";
-    };
+    kato = {system}:
+      pkgs.${system}.buildGoModule rec {
+        name = "kato";
+        src = pkgs.${system}.fetchFromGitHub {
+          owner = "pspiagicw";
+          repo = "kato";
+        };
+      };
   in {
-    packages.x86_64-linux.groom = groom;
+    packages.x86_64-linux.groom = groom {system = "x86_64-linux";};
+    packages.x86_64-linux.kato = kato {system = "x86_64-linux";};
+    packages.aarch64-linux.groom = groom {system = "aarch64-linux";};
+    packages.aarch64-linux.kato = kato {system = "aarch64-linux";};
   };
 }
